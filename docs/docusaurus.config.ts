13e60160f08c8633bd75aaca64464b519332bd72
Thorben <thorben.kuck@gmail.com>
2026-05-08 15:37:32 +0200

    fix: us actions-gh-pages to deploy pages

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -15,15 +15,17 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://github.com',
+  url: 'https://ThorbenKuck.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ThorbenKuck/Vivid',
+  baseUrl: '/Vivid',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ThorbenKuck', // Usually your GitHub org/user name.
   projectName: 'Vivid', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
 
